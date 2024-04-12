b6938296e1b521bc156ca9903e2d951f1334b3b5
Pascall <Pascall@HOME-PC>
2024-04-12 23:26:05 +0300

    Functions

---
+++ Functions/main.cpp
@@ -0,0 +1,41 @@
+﻿//Functions
+#include<iostream>
+using namespace std;
+
+
+int Sum(int a, int b)
+{
+	int c = a + b;
+	return c;
+}
+int Diff(int a, int b)
+{
+	//Difference-разность 
+	return a - b;
+
+}
+
+int Prod(int a, int b)
+{
+	//Product-произведение
+	return a * b;
+}
+
+double Quot(int a, int b)
+{
+	//Quotient-частное
+	return (double)a / b;
+}
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	cout << "Hello Functions" << endl;
+	int a, b;
+	cout << "Введите два числа:"; cin >> a >> b;
+	int c = Sum(a, b);
+	cout << a << " + " << b << " = " << c << endl;
+	cout << a << " - " << b << " = " << Diff(a, b) << endl;
+	cout << a << " * " << b << " = " << Prod(a, b) << endl;
+	cout << a << " / " << b << " = " << Quot(a, b) << endl;
+}
